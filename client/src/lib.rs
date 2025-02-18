a80d34faca3b2a6187aff1a4fa6257a33eb64767
saitewasreset <saite@buaa.edu.cn>
2025-02-18 17:31:38 +0800

    feat(client): add CLI commands

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -4,8 +4,10 @@ use std::fmt::{Display, Formatter};
 use std::hash::RandomState;
 use std::io::Read;
 use std::path::PathBuf;
+use clap::ValueEnum;
 use clio::Input;
 use serde::de::DeserializeOwned;
+use serde::{Deserialize, Serialize};
 use common::admin::APISetMissionInvalid;
 use common::cache::APICacheType;
 use common::kpi::APIDeleteAssignedKPI;
@@ -26,11 +28,44 @@ pub mod mission_invalid;
 pub mod kpi;
 pub mod assigned_kpi;
 
-#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+pub const APP_NAME: &str = env!("CARGO_PKG_NAME");
+pub const APP_VERSION: &str = env!("CARGO_PKG_VERSION");
+pub const APP_DESCRIPTION: &str = env!("CARGO_PKG_DESCRIPTION");
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, Hash)]
 pub struct ClientConfig {
     pub api_endpoint: String,
     pub cookie_path: PathBuf,
     pub mission_raw_log_path: PathBuf,
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, ValueEnum)]
+pub enum CliCacheType {
+    MissionRaw,
+    MissionKPIRaw,
+    GlobalKPIState,
+    All,
+}
+
+impl From<CliCacheType> for APICacheType {
+    fn from(value: CliCacheType) -> Self {
+        match value {
+            CliCacheType::MissionRaw => APICacheType::MissionRaw,
+            CliCacheType::MissionKPIRaw => APICacheType::MissionKPIRaw,
+            CliCacheType::GlobalKPIState => APICacheType::GlobalKPIState,
+            CliCacheType::All => APICacheType::All,
+        }
+    }
+}
+
+impl Default for ClientConfig {
+    fn default() -> Self {
+        Self {
+            api_endpoint: "http://localhost:8080/api".to_string(),
+            cookie_path: PathBuf::from("./cookie.json"),
+            mission_raw_log_path: PathBuf::from("./raw_log"),
+        }
+    }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
@@ -82,7 +117,16 @@ pub fn format_size(size: usize) -> String {
 }
 
 pub fn client_from_local_cookie_unchecked(client_config: ClientConfig) -> Result<MissionMonitorClient<Authenticated>, ClientError> {
-    let cookie_storage_content = std::fs::read(&client_config.cookie_path).map_err(|e| ClientError::InputError(format!("cannot read cookie storage file: {}", e)))?;
+    let cookie_storage_content = match std::fs::read(&client_config.cookie_path) {
+        Ok(content) => content,
+        Err(e) => {
+            return if matches!(e.kind(), std::io::ErrorKind::NotFound) {
+                Err(ClientError::InputError("Please login first".into()))
+            } else {
+                Err(ClientError::InputError(format!("cannot read cookie file: {}", e)))
+            }
+        }
+    };
 
     let client = MissionMonitorClient::<NotAuthenticated>::new(client_config.api_endpoint).load_cookie(&cookie_storage_content).map_err(|(msg, _)| msg)?;
 
@@ -136,11 +180,16 @@ pub fn cli_login(client_config: ClientConfig, token_file: Option<Input>) -> Resu
 }
 
 pub fn cli_load_mission(client_config: ClientConfig) -> Result<(), ClientError> {
+    println!("Parsing mission log...");
     let mission_list = parse_mission_log(&client_config.mission_raw_log_path)?;
 
     let mut client = client_from_local_cookie_unchecked(client_config)?;
 
+    println!("Getting uploaded mission list...");
+
     let uploaded_mission_list = Result::from(client.get_api_mission_list())?;
+
+    println!("uploaded mission count: {}", uploaded_mission_list.len());
 
     let uploaded_mission_timestamp_set: HashSet<_, RandomState> = HashSet::from_iter(uploaded_mission_list.iter().map(|m| m.begin_timestamp));
 
@@ -179,7 +228,7 @@ pub fn cli_get_cache_status(client_config: ClientConfig) -> Result<(), ClientErr
 }
 
 pub fn cli_get_mission_list(client_config: ClientConfig, entry_limit: Option<usize>) -> Result<(), ClientError> {
-    let mut client = client_from_local_cookie_unchecked(client_config)?;
+    let mut client = MissionMonitorClient::new(client_config.api_endpoint);
 
     let api_mission_list = Result::from(client.get_api_mission_list())?;
 
@@ -275,7 +324,7 @@ pub fn cli_server_init(client_config: ClientConfig) -> Result<(), ClientError> {
 }
 
 pub fn cli_get_assigned_kpi(client_config: ClientConfig, mission_id: Option<i32>, player_name: Option<String>) -> Result<(), ClientError> {
-    let mut client = client_from_local_cookie_unchecked(client_config)?;
+    let mut client = MissionMonitorClient::new(client_config.api_endpoint);
 
     let assigned_kpi_list = Result::from(client.get_assigned_kpi())?;
 
@@ -318,4 +367,34 @@ pub fn cli_delete_assigned_kpi(client_config: ClientConfig, mission_id: i32, pla
         mission_id,
         player_name,
     }))
+}
+
+pub fn cli_update_config(mut client_config: ClientConfig, config_path: Option<PathBuf>, api_endpoint: Option<String>, cookie_path: Option<PathBuf>, mission_raw_log_path: Option<PathBuf>) -> Result<(), ClientError> {
+    if let Some(api_endpoint) = api_endpoint {
+        client_config.api_endpoint = api_endpoint.clone();
+    }
+
+    if let Some(cookie_path) = cookie_path {
+        client_config.cookie_path = cookie_path.clone();
+    }
+
+    if let Some(mission_raw_log_path) = mission_raw_log_path {
+        client_config.mission_raw_log_path = mission_raw_log_path.clone();
+    }
+
+    if let Some(config_path) = config_path {
+        confy::store_path(config_path, client_config).map_err(|e| ClientError::InputError(e.to_string()))?;
+    } else {
+        confy::store(APP_NAME, None, client_config).map_err(|e| ClientError::InputError(e.to_string()))?;
+    }
+
+    Ok(())
+}
+
+pub fn cli_print_config(client_config: ClientConfig) -> Result<(), ClientError> {
+    println!("API endpoint: {}", client_config.api_endpoint);
+    println!("Cookie path: {:?}", client_config.cookie_path);
+    println!("Mission raw log path: {:?}", client_config.mission_raw_log_path);
+
+    Ok(())
 }
--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,174 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+use std::path::PathBuf;
+use clap::{Parser, Subcommand};
+use clio::Input;
+use mission_monitor_tools::{APP_NAME, APP_VERSION, APP_DESCRIPTION, cli_print_config, ClientConfig, cli_update_config, CliCacheType, cli_server_init, cli_login, cli_load_mission, cli_load_mapping, cli_load_kpi_config, cli_load_kpi_watchlist, cli_update_cache, cli_get_cache_status, cli_get_mission_list, cli_add_mission_invalid, cli_delete_mission_invalid, cli_get_mission_invalid, cli_set_assigned_kpi, cli_delete_assigned_kpi, cli_get_assigned_kpi};
+
+#[derive(Parser)]
+#[command(name = APP_NAME)]
+#[command(version = APP_VERSION)]
+#[command(about = APP_DESCRIPTION)]
+struct Cli {
+    /// Path to the configuration file
+    #[arg(short, long)]
+    config: Option<PathBuf>,
+
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Get or set the configuration
+    Config {
+        /// API endpoint, e.g. "http://127.0.0.1:8080/api"
+        #[arg(short, long)]
+        api_endpoint: Option<String>,
+
+        /// Path to store the cookie file
+        #[arg(short, long)]
+        cookie_path: Option<PathBuf>,
+
+        /// Path to the directory of mission raw log file
+        #[arg(short, long)]
+        mission_raw_log_path: Option<PathBuf>,
+    },
+    /// Initialize the server
+    ServerInit,
+    /// Login to the server
+    Login {
+        /// File to read access token from
+        token_file: Option<Input>
+    },
+    /// Load mission data
+    LoadMission,
+    /// Load mapping data
+    LoadMapping {
+        /// Path to the directory of mapping files
+        mapping_directory: Option<PathBuf>
+    },
+    /// Load KPI config data
+    LoadKPIConfig {
+        /// Path to the directory of KPI config files
+        kpi_config_directory: Option<PathBuf>
+    },
+    /// Load watchlist data
+    LoadWatchlist {
+        /// Path to the watchlist file
+        watchlist_path: Option<PathBuf>
+    },
+    /// Update server cache
+    UpdateCache {
+        /// Cache type to update
+        #[arg(value_enum)]
+        cache_type: CliCacheType
+    },
+    /// Get server cache status
+    CacheStatus,
+    /// Get mission list
+    MissionList {
+        /// Only show the most recent n entries
+        #[arg(short, long)]
+        entry_limit: Option<usize>
+    },
+    /// Add invalid mark to selected mission
+    AddMissionInvalid {
+        /// Mission ID
+        mission_id: i32,
+        /// Reason for invalid
+        reason: String,
+    },
+    /// Remove invalid mark from selected mission
+    DeleteMissionInvalid {
+        /// Mission ID
+        mission_id: i32
+    },
+    /// Get invalid mark list
+    GetMissionInvalid,
+    /// Add assigned KPI to selected player in selected mission
+    AddAssignedKPI,
+    /// Remove assigned KPI from selected player in selected mission
+    DeleteAssignedKPI {
+        /// Mission ID
+        mission_id: i32,
+        /// Player name
+        player_name: String,
+    },
+    /// Get assigned KPI list
+    GetAssignedKPI {
+        /// Mission ID
+        #[arg(long, short)]
+        mission_id: Option<i32>,
+
+        /// Player name
+        #[arg(long, short)]
+        player_name: Option<String>,
+    },
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let cli = Cli::parse();
+
+    let client_config: ClientConfig = if let Some(config_path) = &cli.config {
+        confy::load_path(config_path)?
+    } else {
+        confy::load(APP_NAME, None)?
+    };
+
+    match cli.command {
+        Commands::Config { api_endpoint, cookie_path, mission_raw_log_path } => {
+            if api_endpoint.is_none() && cookie_path.is_none() && mission_raw_log_path.is_none() {
+                cli_print_config(client_config)?
+            } else {
+                cli_update_config(client_config, cli.config, api_endpoint, cookie_path, mission_raw_log_path)?
+            }
+        }
+        Commands::ServerInit => {
+            cli_server_init(client_config)?
+        }
+        Commands::Login { token_file } => {
+            cli_login(client_config, token_file)?
+        }
+        Commands::LoadMission => {
+            cli_load_mission(client_config)?
+        }
+        Commands::LoadMapping { mapping_directory } => {
+            cli_load_mapping(client_config, mapping_directory)?
+        }
+        Commands::LoadKPIConfig { kpi_config_directory } => {
+            cli_load_kpi_config(client_config, kpi_config_directory)?
+        }
+        Commands::LoadWatchlist { watchlist_path } => {
+            cli_load_kpi_watchlist(client_config, watchlist_path)?
+        }
+        Commands::UpdateCache { cache_type } => {
+            cli_update_cache(client_config, cache_type.into())?
+        }
+        Commands::CacheStatus => {
+            cli_get_cache_status(client_config)?
+        }
+        Commands::MissionList { entry_limit } => {
+            cli_get_mission_list(client_config, entry_limit)?
+        }
+        Commands::AddMissionInvalid { mission_id, reason } => {
+            cli_add_mission_invalid(client_config, mission_id, reason)?
+        }
+        Commands::DeleteMissionInvalid { mission_id } => {
+            cli_delete_mission_invalid(client_config, mission_id)?
+        }
+        Commands::GetMissionInvalid => {
+            cli_get_mission_invalid(client_config)?
+        }
+        Commands::AddAssignedKPI => {
+            cli_set_assigned_kpi(client_config)?
+        }
+        Commands::DeleteAssignedKPI { mission_id, player_name } => {
+            cli_delete_assigned_kpi(client_config, mission_id, player_name)?
+        }
+        Commands::GetAssignedKPI { mission_id, player_name } => {
+            cli_get_assigned_kpi(client_config, mission_id, player_name)?
+        }
+    };
+
+    Ok(())
 }
